13bffa24e9298ef28807279db7771c4b940c8515
Pavel-Liteiniy <paulsev96@gmail.com>
2021-02-16 00:00:30 +0300

    Добавляет утилитарную функцию для удаления элемента массива

--- src/utils/common.js
+++ src/utils/common.js
@@ -10,6 +10,17 @@ export const updateItem = (items, update, index) => {
   ];
 };
 
+export const deleteItem = (items, index) => {
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    ...items.slice(index + 1)
+  ];
+};
+
 export const isOnline = () => {
   return window.navigator.onLine;
 };
