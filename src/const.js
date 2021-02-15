69718cb59784d2642d18c9ccb14bb893576e3b11
Pavel-Liteiniy <paulsev96@gmail.com>
2021-02-16 00:02:34 +0300

    Создает файл с константными значениями

---
+++ src/const.js
@@ -0,0 +1,37 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const UpdateType = {
+  PATCH: `PATCH`,
+  MINOR: `MINOR`,
+  MAJOR: `MAJOR`,
+  STATS: `STATS`,
+  INIT: `INIT`,
+};
+
+export const SortType = {
+  BY_DAY: `sort-day`,
+  BY_TIME: `sort-time`,
+  BY_PRICE: `sort-price`,
+};
+
+export const FilterType = {
+  EVERYTHING: `everything`,
+  FUTURE: `future`,
+  PAST: `past`,
+};
+
+export const EventType = {
+  TAXI: `taxi`,
+  BUS: `bus`,
+  TRAIN: `train`,
+  SHIP: `ship`,
+  TRANSPORT: `transport`,
+  DRIVE: `drive`,
+  FLIGHT: `flight`,
+  CHECK_IN: `check-in`,
+  SIGHTSEEING: `sightseeing`,
+  RESTAURANT: `restaurant`,
+};
